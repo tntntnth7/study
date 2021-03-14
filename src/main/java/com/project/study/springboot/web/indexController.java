2098bbb2a220cb97c5d7c96b11c87fbbf4c7924a
tntntnth7 <tntntnth@naver.com>
2021-03-14 20:16:51 +0900

    20210314_final

--- src/main/java/com/project/study/springboot/web/indexController.java
+++ src/main/java/com/project/study/springboot/web/indexController.java
@@ -1,10 +1,12 @@
 package com.project.study.springboot.web;
 
 import com.project.study.springboot.service.posts.PostsService;
+import com.project.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +23,14 @@ public class indexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
     }
 
 }
