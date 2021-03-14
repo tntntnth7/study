2b8d0cbb3da10a80ae9c18ad7d2262925dff9a0d
tntntnth7 <tntntnth@naver.com>
2021-03-14 17:22:32 +0900

    20210314

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,19 +1,11 @@
 var main = {
     init : function () {
         var _this = this;
-<<<<<<< HEAD
         $('#btn-save').on('click', function (){
             _this.save();
         });
     },
-    save : function (){
-=======
-        $('#btn-save').on('click',function (){
-            _this.save();
-        });
-    },
     save : function(){
->>>>>>> origin/master
         var data = {
             title : $('#title').val(),
             author : $('#author').val(),
@@ -24,19 +16,11 @@ var main = {
             type : 'POST',
             url : '/api/v1/posts',
             dataType : 'json',
-<<<<<<< HEAD
             contentType : 'application/json; charset=utf-8',
-            data : JSON.stringify(data)
-        }).done(function(){
-            alert("글이 등록되었습니다.");
-            window.location.href = '/';
-=======
-            contentType : 'application/json; charset=utr-8',
             data : JSON.stringify(data)
         }).done(function(){
             alert('글이 등록되었습니다.');
             window.location.href = "/";
->>>>>>> origin/master
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
